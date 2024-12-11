93026d099f5a83ae60ef75b67191704c03fabb97
Spyridoula Kastanou <13-13907@saeinstitute.edu>
2024-12-11 21:03:54 +0200

    test1

---
+++ script.js
@@ -0,0 +1,15 @@
+// const arr=[1,2,4,6,10,20];
+
+// const arr2=arr.slice(0 , 4); 
+
+
+// console.log('This is arr: ' , arr);
+// console.log('This is arr2: ' , arr2);
+
+
+const arr=[1,2,4,6,10,20];
+
+const arr2=arr.splice(0 , 4); 
+
+console.log('This is arr: ' , arr);
+console.log('This is arr2: ' , arr2);
